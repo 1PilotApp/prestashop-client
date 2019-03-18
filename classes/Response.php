4d637d9c383ed7cecd33dedec5bec119e71d786a
Christophe Vuagniaux <ChVuagniaux@users.noreply.github.com>
2019-03-18 10:31:29 +0100

    Set content-type

--- classes/Response.php
+++ classes/Response.php
@@ -5,6 +5,7 @@ class Response
     public static function make($data, $status = 200)
     {
         http_response_code($status);
+        header('Content-Type: application/json');
 
         exit(json_encode($data));
     }
