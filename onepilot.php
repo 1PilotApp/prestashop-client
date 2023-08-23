f239448ef9ac32ab00821d5b5b53e3b9b8ca194b
Christophe Vuagniaux <christophe@inetis.ch>
2023-08-23 10:50:03 +0200

    Signatures updates

--- onepilot.php
+++ onepilot.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 2007-2019 PrestaShop
+ * Copyright (c) since 2018 1Pilot (https://1pilot.io)
  *
  * NOTICE OF LICENSE
  *
@@ -12,16 +12,9 @@
  * obtain it through the world-wide-web, please send an email
  * to license@prestashop.com so we can send you a copy immediately.
  *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
- * versions in the future. If you wish to customize PrestaShop for your
- * needs please refer to http://www.prestashop.com for more information.
- *
- * @author    PrestaShop SA <contact@prestashop.com>
- * @copyright 2007-2019 PrestaShop SA
+ * @author    1Pilot <support@1pilot.io>
+ * @copyright 1Pilot.io
  * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
- *  International Registered Trademark & Property of PrestaShop SA
  */
 
 if (!defined('_PS_VERSION_')) {
